35af31c59e2caa925910049addf889af68fa1c49
Melvin-Jovano <melvinjovano2@gmail.com>
2024-11-19 16:13:59 +0700

    feat: tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js}', // Include all HTML and JS files in the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
