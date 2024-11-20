45fa12327c49368cd5eebf9c85f084a5ef045344
Melvin-Jovano <melvinjovano2@gmail.com>
2024-11-20 19:43:28 +0700

    feat: update HTML

--- sql/records-overview/recently_added.sql
+++ sql/records-overview/recently_added.sql
@@ -1,10 +1,22 @@
-SELECT TOP (20) Audit.AuditDate, KUAF.Name "UserName", KUAF.FirstName, Audit.Value2 "FileName",
-KUAF.LastName, DTreeCore.ParentID "FolderID", DtreeCore.DataID "FileID", 
-DTreeCoreFolder.Name "FolderName"
-FROM DAuditNewCore "Audit"
-LEFT JOIN KUAF "KUAF" ON KUAF.ID = Audit.PerformerID
-LEFT JOIN DTreeCore "DTreeCore" ON Audit.DataID = DtreeCore.DataID
-LEFT JOIN DTreeCore "DTreeCoreFolder" ON DtreeCore.ParentID = "DTreeCoreFolder".DataID
-WHERE Audit.AuditID = 1 
-AND Audit.SubType = 144
-ORDER BY Audit.AuditDate DESC
+SELECT 
+    Audit.AuditDate, 
+    KUAF.Name AS UserName, 
+    KUAF.FirstName, 
+    Audit.Value2 AS FileName,
+    KUAF.LastName, 
+    DTreeCore.ParentID AS FolderID, 
+    DTreeCore.DataID AS FileID, 
+    DTreeCoreFolder.Name AS FolderName
+FROM 
+    DAuditNewCore AS Audit
+LEFT JOIN 
+    KUAF ON KUAF.ID = Audit.PerformerID
+LEFT JOIN 
+    DTreeCore ON Audit.DataID = DTreeCore.DataID
+LEFT JOIN 
+    DTreeCore AS DTreeCoreFolder ON DTreeCore.ParentID = DTreeCoreFolder.DataID
+WHERE 
+    Audit.AuditID = 1 
+    AND Audit.SubType = 144
+ORDER BY 
+    Audit.AuditDate DESC;
